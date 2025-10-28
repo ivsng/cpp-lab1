8734948bd0955757666206dba7a415692527c753
ivsng <167986711+ivsng@users.noreply.github.com>
2025-10-28 14:29:22 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include "lib.h"
-
 #include <iostream>
 
 int main (int, char **) {
     std::cout << "Version: " << version() << std::endl;
-    std::cout << "Hello, world!" << std::endl;
+    std::cout << "Hello, World! Version 1.0." << version() << std::endl;
+    
+    // Добавь эти строки для паузы:
+    std::cout << "Press Enter to exit..." << std::endl;
+    std::cin.get();
+    
     return 0;
 }
